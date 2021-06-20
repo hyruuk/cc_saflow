62ed7c31073695a762d929deb98aff5e51679b4f
Yann Harel <yharel109@gmail.com>
2021-06-20 01:17:53 -0400

    cleaned paths

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -2,7 +2,7 @@ import os
 import os.path as op
 from src.utils import get_SAflow_bids
 from src.neuro import saflow_preproc, find_rawfile
-from src.saflow_params import FOLDERPATH, SUBJ_LIST, BLOCS_LIST, REPORTS_PATH
+from src.saflow_params import BIDS_PATH, SUBJ_LIST, BLOCS_LIST
 
 
 if __name__ == "__main__":
@@ -13,10 +13,8 @@ if __name__ == "__main__":
 		print('Report path already exists.')
 	for subj in SUBJ_LIST:
 		for bloc in BLOCS_LIST:
-			filepath, filename = find_rawfile(subj, bloc, FOLDERPATH)
-			save_pattern = get_SAflow_bids(FOLDERPATH, subj=subj, run=bloc, stage='preproc_raw')[1]
-			#save_pattern =  op.join(FOLDERPATH + filepath, filename[:-3] + '_preproc_raw.fif.gz')
-			report_pattern = get_SAflow_bids(FOLDERPATH, subj=subj, run=bloc, stage='_preproc_report.html')[1]
-			#report_pattern = op.join(REPORTS_PATH, filename[:-3] + '_preproc_report.html')
-			full_filepath = FOLDERPATH + filepath + filename
-			saflow_preproc(full_filepath, save_pattern, report_pattern)
+			filepath, filename = find_rawfile(subj, bloc, BIDS_PATH)
+			save_path = get_SAflow_bids(BIDS_PATH, subj=subj, run=bloc, stage='preproc_raw')[1]
+			report_path = get_SAflow_bids(BIDS_PATH, subj=subj, run=bloc, stage='_preproc_report.html')[1]
+			full_filepath = BIDS_PATH + filepath + filename
+			saflow_preproc(full_filepath, save_path, report_path)
