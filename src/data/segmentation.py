7474ab4d1f4a94355747efa020a1418533450467
Yann Harel <yharel109@gmail.com>
2021-06-21 21:08:30 -0400

    corrected path

--- src/data/segmentation.py
+++ src/data/segmentation.py
@@ -1,16 +1,16 @@
 ##### OPEN PREPROC FILES AND SEGMENT THEM
 from src.utils import get_SAflow_bids
 from src.neuro import segment_files
-from src.saflow_params import FOLDERPATH, LOGS_DIR, SUBJ_LIST, BLOCS_LIST
+from src.saflow_params import BIDS_PATH, LOGS_DIR, SUBJ_LIST, BLOCS_LIST
 import pickle
 
 
 if __name__ == "__main__":
     for subj in SUBJ_LIST:
         for bloc in BLOCS_LIST:
-            preproc_path, preproc_filename = get_SAflow_bids(FOLDERPATH, subj, bloc, stage='preproc_raw', cond=None)
-            epoched_path, epoched_filename = get_SAflow_bids(FOLDERPATH, subj, bloc, stage='-epo', cond=None)
-            ARlog_path, ARlog_filename = get_SAflow_bids(FOLDERPATH, subj, bloc, stage='ARlog', cond=None)
+            preproc_path, preproc_filename = get_SAflow_bids(BIDS_PATH, subj, bloc, stage='preproc_raw', cond=None)
+            epoched_path, epoched_filename = get_SAflow_bids(BIDS_PATH, subj, bloc, stage='-epo', cond=None)
+            ARlog_path, ARlog_filename = get_SAflow_bids(BIDS_PATH, subj, bloc, stage='ARlog', cond=None)
             epochs_clean, AR_log = segment_files(preproc_filename, tmin=0, tmax=0.8)
             epochs_clean.save(epoched_filename, overwrite=True)
             del epochs_clean
