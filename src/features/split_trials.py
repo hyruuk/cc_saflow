b71f016e1a6e35883170e13046226c41ec402cfc
Clara EL KHANTOUR <clara.elkhantour@gmail.com>
2021-07-01 13:04:18 -0400

    Ajout de split_trials

--- src/features/split_trials.py
+++ src/features/split_trials.py
@@ -1,12 +1,20 @@
 ##### OPEN PREPROC FILES AND SEGMENT THEM
-from neuro import split_PSD_data
-from saflow_params import FOLDERPATH, SUBJ_LIST, BLOCS_LIST, FEAT_PATH
+from src.neuro import split_PSD_data, split_trials
+from src.saflow_params import FOLDERPATH, SUBJ_LIST, BLOCS_LIST, FEAT_PATH, BIDS_PATH, LOGS_DIR
+from src.utils import get_SAflow_bids
 from scipy.io import savemat
 import pickle
 
 if __name__ == "__main__":
-    PSD_alldata = split_PSD_data(FOLDERPATH, SUBJ_LIST, BLOCS_LIST, by='odd', lobound=0.25, hibound=0.75, stage='PSD', filt_order=3, filt_cutoff=0.1)
-    PSD_dict = {'PSD_alldata': PSD_alldata}
+    for subj in SUBJ_LIST:
+        for run in BLOCS_LIST:
 
-    with open(FEAT_PATH + 'PSD_oddball', 'wb') as fp:
-        pickle.dump(PSD_alldata, fp)
+            INepochs, OUTepochs = split_trials(BIDS_PATH, LOGS_DIR, subj=subj, run=run, stage='PSD', by='VTC')
+            INepochs_path, INepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='IN')
+            OUTepochs_path, OUTepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='OUT')
+            
+            with open(INepochs_filename, 'wb') as fp:
+                pickle.dump(INepochs, fp)
+
+            with open(OUTepochs_filename, 'wb') as fp:
+                pickle.dump(OUTepochs, fp)
