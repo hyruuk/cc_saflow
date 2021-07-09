fcccaf917aa45a10b06578d76a9d7b3949549ccc
Yann Harel <yharel109@gmail.com>
2021-07-09 15:12:18 -0400

    removed get_ch_pos and cleaned array_topoplot

--- src/utils.py
+++ src/utils.py
@@ -35,25 +35,26 @@ def get_SAflow_bids(BIDS_PATH, subj, run, stage, cond=None):
     SAflow_bidspath = os.path.join(BIDS_PATH, 'sub-{}'.format(subj), 'ses-recording', 'meg', SAflow_bidsname)
     return SAflow_bidsname, SAflow_bidspath
 
-
-
 def array_topoplot(toplot, ch_xy, showtitle=False, titles=None, savefig=False, figpath=None, vmin=-1, vmax=1, cmap='magma', with_mask=False, masks=None, show=True):
     #create fig
     mask_params = dict(marker='o', markerfacecolor='w', markeredgecolor='k', linewidth=0, markersize=5)
-    fig, ax = plt.subplots(1,len(toplot), figsize=(20,10))
+    fig, ax = plt.subplots(1,len(toplot), figsize=(20,10), squeeze=False)
     for i, data in enumerate(toplot):
         if with_mask == False:
-            image,_ = mne.viz.plot_topomap(data=data, pos=ch_xy, cmap=cmap, vmin=vmin, vmax=vmax, axes=ax[i], show=False, contours=None, extrapolate='box', outlines='head')
+            image,_ = mne.viz.plot_topomap(data=data, pos=ch_xy, cmap=cmap, vmin=vmin, vmax=vmax, axes=ax[0][i], show=False, contours=None, extrapolate='box', outlines='head', sphere=0.15)
         elif with_mask == True:
-            image,_ = mne.viz.plot_topomap(data=data, pos=ch_xy, cmap=cmap, vmin=vmin, vmax=vmax, axes=ax[i], show=False, contours=None, mask_params=mask_params, mask=masks[i], extrapolate='box', outlines='head')
+            image,_ = mne.viz.plot_topomap(data=data, pos=ch_xy, cmap=cmap, vmin=vmin, vmax=vmax, axes=ax[0][i], show=False, contours=None, mask_params=mask_params, mask=masks[i], extrapolate='box', outlines='head', sphere=0.15)
+
         #option for title
         if showtitle == True:
             ax[i].set_title(titles[i], fontdict={'fontsize': 20, 'fontweight': 'heavy'})
+
     #add a colorbar at the end of the line (weird trick from https://www.martinos.org/mne/stable/auto_tutorials/stats-sensor-space/plot_stats_spatio_temporal_cluster_sensors.html#sphx-glr-auto-tutorials-stats-sensor-space-plot-stats-spatio-temporal-cluster-sensors-py)
-    divider = make_axes_locatable(ax[-1])
+    divider = make_axes_locatable(ax[0][-1])
     ax_colorbar = divider.append_axes('right', size='5%', pad=0.05)
     plt.colorbar(image, cax=ax_colorbar)
     ax_colorbar.tick_params(labelsize=14)
+
     #save plot if specified
     if savefig == True:
         plt.savefig(figpath, dpi=300)
