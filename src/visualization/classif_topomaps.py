068a25db0c21538cccf3f8cdbe1df733a62d225a
Clara EL KHANTOUR <clara.elkhantour@gmail.com>
2021-07-22 11:16:22 -0400

    Combined single and multi feature

--- src/visualization/classif_topomaps.py
+++ src/visualization/classif_topomaps.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "-n",
     "--classif_name",
-    default='LDAsf_LOGO_100perm',
+    default='VTC_LDAsf_LOGO_1perm_2575',
     type=str,
     help="Name of the folder that contains classification results",
 )
@@ -26,7 +26,6 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-
 if __name__ == "__main__":
     classif_name = args.classif_name
     alpha = args.alpha
@@ -37,12 +36,44 @@ if __name__ == "__main__":
     allfreqs_pval = []
     allmasks = []
 
-    for FREQ in FREQS_NAMES:
+    if 'sf_' in classif_name:
+        for FREQ in FREQS_NAMES:
+            allchans_acc = []
+            allchans_pval = []
+            allchans_accperms = []
+            for CHAN in range(270):
+                savename = 'chan_{}_{}.pkl'.format(CHAN, FREQ)
+                with open(savepath + savename, 'rb') as f:
+                    result = pickle.load(f)
+                allchans_acc.append(result['acc_score'])
+                allchans_pval.append(result['acc_pvalue'])
+                allchans_accperms.append(result['acc_pscores'])
+
+            # Correction for multiple comparisons
+            freq_perms = list(itertools.chain.from_iterable(allchans_accperms))
+            corrected_pval = []
+            for acc in allchans_acc:
+                corrected_pval.append(compute_pval(acc[0], freq_perms))
+            pval_mask = create_pval_mask(np.array(corrected_pval), alpha=alpha)
+
+            allfreqs_acc.append(np.array(allchans_acc).squeeze())
+            allfreqs_pval.append(np.array(allchans_pval).squeeze()) 
+            allmasks.append(pval_mask)
+
+        toplot_pval = allfreqs_pval
+        figpath_pval = IMG_DIR + classif_name + '_pval' + str(alpha)[2:] + '.png'
+
+        toplot_acc = allfreqs_acc
+        figpath_acc = IMG_DIR + classif_name + '_acc' + str(alpha)[2:] + '.png'
+
+        titles = FREQS_NAMES
+
+    elif 'mf_' in classif_name:
         allchans_acc = []
         allchans_pval = []
         allchans_accperms = []
         for CHAN in range(270):
-            savename = 'chan_{}_{}.pkl'.format(CHAN, FREQ)
+            savename = 'chan_{}.pkl'.format(CHAN)
             with open(savepath + savename, 'rb') as f:
                 result = pickle.load(f)
             allchans_acc.append(result['acc_score'])
@@ -56,15 +87,17 @@ if __name__ == "__main__":
             corrected_pval.append(compute_pval(acc[0], freq_perms))
         pval_mask = create_pval_mask(np.array(corrected_pval), alpha=alpha)
 
-        allfreqs_acc.append(np.array(allchans_acc).squeeze()) #array, shape (n_chan,)
-        allfreqs_pval.append(np.array(allchans_pval).squeeze()) #array, shape (n_chan,)
+        allfreqs_acc.append(np.array(allchans_acc).squeeze()) 
+        allfreqs_pval.append(np.array(allchans_pval).squeeze()) 
         allmasks.append(pval_mask)
 
-    toplot_pval = allfreqs_pval
-    figpath_pval = IMG_DIR + classif_name + '_pval' + str(alpha)[2:] + '.png'
+        toplot_pval = allfreqs_pval
+        figpath_pval = IMG_DIR + classif_name + '_pval' + str(alpha)[2:] + '.png'
 
-    toplot_acc = allfreqs_acc
-    figpath_acc = IMG_DIR + classif_name + '_acc' + str(alpha)[2:] + '.png'
+        toplot_acc = allfreqs_acc
+        figpath_acc = IMG_DIR + classif_name + '_acc' + str(alpha)[2:] + '.png'
+
+        titles =['All Frequencies']
 
     _, data_fname = get_SAflow_bids(BIDS_PATH, subj='04', run='2', stage='-epo')
     epochs = mne.read_epochs(data_fname)
@@ -76,10 +109,10 @@ if __name__ == "__main__":
     vmax_acc = np.max(np.max(np.asarray(toplot_acc)))
     vmin_acc = np.min(np.min(np.asarray(toplot_acc)))
 
-    array_topoplot(toplot_pval, ch_xy, showtitle=True, titles=FREQS_NAMES,
+    array_topoplot(toplot_pval, ch_xy, showtitle=True, titles=titles,
                     savefig=True, figpath=figpath_pval, vmin=vmin_pval, vmax=vmax_pval,
                     with_mask=True, masks=allmasks)
 
-    array_topoplot(toplot_acc, ch_xy, showtitle=True, titles=FREQS_NAMES,
+    array_topoplot(toplot_acc, ch_xy, showtitle=True, titles=titles,
                     savefig=True, figpath=figpath_acc, vmin=vmin_acc, vmax=vmax_acc,
                     with_mask=True, masks=allmasks)
