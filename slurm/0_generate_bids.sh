4d4f9bd693fdaf49caa4143ebda1b2af7bea2163
hyruuk lab <yharel109@gmail.com>
2021-09-03 12:06:19 -0400

    created submission file for generate_bids

---
+++ slurm/0_generate_bids.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --time=12:00:00
+#SBATCH --job-name=saflow_generate_bids
+#SBATCH --mem=31G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+
+module load python/3.8.0
+
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/data/generate_bids.py
