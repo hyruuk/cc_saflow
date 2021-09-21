5d79a8f4d1b124759ec2308e06e193aebe3c6168
Hyruuk <yharel109@gmail.com>
2021-09-21 14:02:35 -0400

    created submission file for preprocessing

--- slurm/1_preprocessing.sh
+++ slurm/1_preprocessing.sh
@@ -1,10 +1,10 @@
-  #!/bin/bash
-  #SBATCH --account=def-kjerbi
-  #SBATCH --time=12:00:00
-  #SBATCH --job-name=saflow_generate_bids
-  #SBATCH --mem=31G
-  #SBATCH --nodes=1
-  #SBATCH --ntasks-per-node=12
+#!/bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --time=12:00:00
+#SBATCH --job-name=saflow_generate_bids
+#SBATCH --mem=31G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
 
 for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25; do
   module load python/3.7.0
