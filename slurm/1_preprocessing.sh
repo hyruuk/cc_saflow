b4871411bebbacf987886fef9a3801e472022474
Hyruuk <yharel109@gmail.com>
2021-09-21 14:01:59 -0400

    created submission file for preprocessing

--- slurm/1_preprocessing.sh
+++ slurm/1_preprocessing.sh
@@ -1,5 +1,4 @@
   #!/bin/bash
-  for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25; do
   #SBATCH --account=def-kjerbi
   #SBATCH --time=12:00:00
   #SBATCH --job-name=saflow_generate_bids
@@ -7,6 +6,7 @@
   #SBATCH --nodes=1
   #SBATCH --ntasks-per-node=12
 
+for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25; do
   module load python/3.7.0
 
   $HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/data/preprocessing.py -s $SUB
