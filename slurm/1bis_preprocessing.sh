e054de05d21130e80a1ba019ea5e43f63fc4c45f
Hyruuk <yharel109@gmail.com>
2021-09-21 14:21:50 -0400

    cleaned

--- slurm/1bis_preprocessing.sh
+++ slurm/1bis_preprocessing.sh
@@ -6,8 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
 
-for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25; do
+
 module load python/3.7.0
 
 $HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/data/preprocessing.py -s $1
-done
