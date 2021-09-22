dff537a2584e2effb9b1e654f3176f638cd3cf3c
Hyruuk <yharel109@gmail.com>
2021-09-22 11:26:28 -0400

    created subm files for segmentation

--- slurm/2_segmentation.sh
+++ slurm/2_segmentation.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-#SBATCH --account=def-kjerbi
-#SBATCH --time=12:00:00
-#SBATCH --job-name=saflow_generate_bids
-#SBATCH --mem=512G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
-
-
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/data/segmentation.py
+for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25; do
+  sbatch ./slurm/2bis_segmentation.sh $SUB
+done
