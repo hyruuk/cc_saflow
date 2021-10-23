9bd1c26eb0b847c959e39cc36b27465f021a331b
Clara EL KHANTOUR <clara.elkhantour@gmail.com>
2021-10-23 12:19:24 -0400

    Added slurm files for classif and topomaps

---
+++ slurm/6_classif_singlefeat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --time=12:00:00
+#SBATCH --job-name=saflow_generate_bids
+#SBATCH --mem=512G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/models/classif_singlefeat.py -m LR
