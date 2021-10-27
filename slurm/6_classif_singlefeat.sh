4776098a730e19fdb4a8f93e963e6c3881c08fd0
Hyruuk <yharel109@gmail.com>
2021-10-26 22:39:04 -0400

    added batch for singlefeat

--- slurm/6_classif_singlefeat.sh
+++ slurm/6_classif_singlefeat.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=def-kjerbi
 #SBATCH --time=12:00:00
 #SBATCH --job-name=saflow_generate_bids
-#SBATCH --mem=512G
+#SBATCH --mem=4G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=8
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/models/classif_singlefeat.py -m LR
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/src/models/classif_singlefeat.py -m LR -c $1
