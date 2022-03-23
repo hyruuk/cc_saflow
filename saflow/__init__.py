5e9ce88440825bd14d9b5e41c19e2ae88a8c713a
Hyruuk <yharel109@gmail.com>
2022-03-23 11:28:12 -0400

    changed paths for narval

--- saflow/__init__.py
+++ saflow/__init__.py
@@ -1,5 +1,5 @@
-BIDS_PATH = '/media/storage/MEG_DATA/saflow/bids/'
-ACQ_PATH = '/storage/MEG_DATA/saflow/acquisition/'
+BIDS_PATH = '/scratch/hyruuk/saflow_data/bids/'
+ACQ_PATH = '/scratch/hyruuk/saflow_data/acquisition/'
 
 FOLDERPATH = '/home/hyruuk/GitHub/cc_saflow/'
 RESULTS_PATH = './results/'
@@ -10,7 +10,7 @@ LOGS_DIR = '/home/karim/pCloudDrive/science/saflow/gradCPT/gradCPT_share_Mac_PC/
 FEAT_PATH = './features/'
 FREQS = [ [2, 4], [4, 8], [8, 12], [12, 20], [20, 30], [30, 60], [60, 90], [90, 120] ]
 FREQS_NAMES = ['delta', 'theta', 'alpha', 'lobeta', 'hibeta', 'gamma1', 'gamma2', 'gamma3']
-SUBJ_LIST = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
+SUBJ_LIST = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38']
 BLOCS_LIST = ['2','3', '4', '5', '6', '7']
 ZONE_CONDS = ['IN', 'OUT']
 ZONE2575_CONDS = ['IN25', 'OUT75']
