97520946bde12ff2e0c3c3838aed581910c9e80d
Hyruuk <yharel109@gmail.com>
2022-03-26 01:18:56 -0400

    prepared singlefeat

--- slurm/6_classif_singlefeat.sh
+++ slurm/6_classif_singlefeat.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/classif_singlefeat.py -m LDA -c $1 -f $2 -by resp
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/classif_singlefeat.py -m LDA -c $1 -f $2 -by VTC
--- slurm/6batch_classif_singlefeat.sh
+++ slurm/6batch_classif_singlefeat.sh
@@ -1,3 +1,5 @@
+for BAND in delta theta alpha lobeta hibeta gamma1 gamma2 gamma3; do
 for CHAN in {0..270}; do
-  sbatch ./slurm/6_classif_singlefeat.sh $CHAN delta
+  sbatch ./slurm/6_classif_singlefeat.sh $CHAN $BAND
+done
 done
