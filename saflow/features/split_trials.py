b99020aff8569a01c53c9a87b0fb0668cfe3d9c1
Hyruuk <yharel109@gmail.com>
2022-03-26 01:15:26 -0400

    blacked

--- saflow/features/split_trials.py
+++ saflow/features/split_trials.py
@@ -12,7 +12,7 @@ parser.add_argument(
     "--split",
     default=[25, 75],
     type=int,
-    nargs='+',
+    nargs="+",
     help="Bounds of percentile split",
 )
 parser.add_argument(
@@ -32,33 +32,71 @@ if __name__ == "__main__":
             CONDS_LIST = args.split
             by = args.by
 
-            if by == 'VTC':
-                INepochs, OUTepochs = split_trials(BIDS_PATH, LOGS_DIR, subj=subj, run=run, stage='PSD', by='VTC', lobound=CONDS_LIST[0], hibound=CONDS_LIST[1])
-                INepochs_path, INepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='IN{}'.format(CONDS_LIST[0]))
-                OUTepochs_path, OUTepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='OUT{}'.format(CONDS_LIST[1]))
+            if by == "VTC":
+                INepochs, OUTepochs = split_trials(
+                    BIDS_PATH,
+                    LOGS_DIR,
+                    subj=subj,
+                    run=run,
+                    stage="PSD",
+                    by="VTC",
+                    lobound=CONDS_LIST[0],
+                    hibound=CONDS_LIST[1],
+                )
+                INepochs_path, INepochs_filename = get_SAflow_bids(
+                    BIDS_PATH,
+                    subj=subj,
+                    run=run,
+                    stage="PSD",
+                    cond="IN{}".format(CONDS_LIST[0]),
+                )
+                OUTepochs_path, OUTepochs_filename = get_SAflow_bids(
+                    BIDS_PATH,
+                    subj=subj,
+                    run=run,
+                    stage="PSD",
+                    cond="OUT{}".format(CONDS_LIST[1]),
+                )
 
-                with open(INepochs_filename, 'wb') as fp:
+                with open(INepochs_filename, "wb") as fp:
                     pickle.dump(INepochs, fp)
-                with open(OUTepochs_filename, 'wb') as fp:
+                with open(OUTepochs_filename, "wb") as fp:
                     pickle.dump(OUTepochs, fp)
 
-            elif by == 'odd':
-                FREQepochs, RAREepochs = split_trials(BIDS_PATH, LOGS_DIR, subj=subj, run=run, stage='PSD', by='odd', oddball='hits')
-                FREQepochs_path, FREQepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='FREQhits')
-                RAREepochs_path, RAREepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='RAREhits')
+            elif by == "odd":
+                FREQepochs, RAREepochs = split_trials(
+                    BIDS_PATH,
+                    LOGS_DIR,
+                    subj=subj,
+                    run=run,
+                    stage="PSD",
+                    by="odd",
+                    oddball="hits",
+                )
+                FREQepochs_path, FREQepochs_filename = get_SAflow_bids(
+                    BIDS_PATH, subj=subj, run=run, stage="PSD", cond="FREQhits"
+                )
+                RAREepochs_path, RAREepochs_filename = get_SAflow_bids(
+                    BIDS_PATH, subj=subj, run=run, stage="PSD", cond="RAREhits"
+                )
 
-                with open(FREQepochs_filename, 'wb') as fp:
+                with open(FREQepochs_filename, "wb") as fp:
                     pickle.dump(FREQepochs, fp)
-                with open(RAREepochs_filename, 'wb') as fp:
+                with open(RAREepochs_filename, "wb") as fp:
                     pickle.dump(RAREepochs, fp)
 
+            elif by == "resp":
+                RESPepochs, NORESPepochs = split_trials(
+                    BIDS_PATH, LOGS_DIR, subj=subj, run=run, stage="PSD", by="resp"
+                )
+                RESPepochs_path, RESPepochs_filename = get_SAflow_bids(
+                    BIDS_PATH, subj=subj, run=run, stage="PSD", cond="RESP"
+                )
+                NORESPepochs_path, NORESPepochs_filename = get_SAflow_bids(
+                    BIDS_PATH, subj=subj, run=run, stage="PSD", cond="NORESP"
+                )
 
-            elif by == 'resp':
-                RESPepochs, NORESPepochs = split_trials(BIDS_PATH, LOGS_DIR, subj=subj, run=run, stage='PSD', by='resp')
-                RESPepochs_path, RESPepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='RESP')
-                NORESPepochs_path, NORESPepochs_filename = get_SAflow_bids(BIDS_PATH, subj=subj, run=run, stage='PSD', cond='NORESP')
-
-                with open(RESPepochs_filename, 'wb') as fp:
+                with open(RESPepochs_filename, "wb") as fp:
                     pickle.dump(RESPepochs, fp)
-                with open(NORESPepochs_filename, 'wb') as fp:
+                with open(NORESPepochs_filename, "wb") as fp:
                     pickle.dump(NORESPepochs, fp)
