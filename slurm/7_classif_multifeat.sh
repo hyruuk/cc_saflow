92e30c84ed2085c833c4b517f148ba74d4f7091e
Hyruuk <yharel109@gmail.com>
2022-03-28 02:02:11 -0400

    compute VTC

--- slurm/7_classif_multifeat.sh
+++ slurm/7_classif_multifeat.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/classif_multifeat.py -m LDA -c $1 -by resp
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/classif_multifeat.py -m LDA -c $1 -by VTC
