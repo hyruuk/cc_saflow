a145b54ec05b1617709f3a08de4f51cb86f54ad2
Hyruuk <yharel109@gmail.com>
2022-03-28 13:34:15 -0400

    ready to run LR singlefeat

--- slurm/6batch_classif_singlefeat.sh
+++ slurm/6batch_classif_singlefeat.sh
@@ -1,4 +1,4 @@
-for BAND in delta theta alpha lobeta hibeta gamma1 gamma2 gamma3; do
+for BAND in delta theta alpha; do
 for CHAN in {0..270}; do
   sbatch ./slurm/6_classif_singlefeat.sh $CHAN $BAND
 done
