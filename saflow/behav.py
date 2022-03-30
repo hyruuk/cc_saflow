88fbc0569364e3036ab097446821db793152e07f
Hyruuk <yharel109@gmail.com>
2022-03-30 00:30:35 -0400

    updated VTC functions

--- saflow/behav.py
+++ saflow/behav.py
@@ -20,26 +20,6 @@ def find_logfile(subj, bloc, log_files):
     return file
 
 
-def interp_RT(RT):
-    ### Interpolate missing reaction times using the average of proximal values.
-    # Note that this technique behaves poorly when two 0 are following each other
-    for i in range(len(RT)):
-        if RT[i] == 0:
-            try:
-                if RT[i + 1] != 0 and RT[i - 1] != 0:
-                    RT[i] = np.mean((RT[i - 1], RT[i + 1]))
-                else:
-                    RT[i] = RT[i - 1]
-
-            except:  # if RT[i+1] or RT[i-1] doesn't exists (beginning or end of file) replace by nearest RT
-                try:
-                    RT[i] = RT[i - 1]
-                except:
-                    RT[i] = RT[i + 1]
-    RT_interpolated = RT
-    return RT_interpolated
-
-
 def interpolate_RT(RT_raw):
     """Interpolates missing reaction times from the two nearest RTs.
 
@@ -71,87 +51,65 @@ def interpolate_RT(RT_raw):
     return RT_array
 
 
-def old_compute_VTC(RT_interp, filt=True, filt_order=3, filt_cutoff=0.05):
-    ### Compute the variance time course (VTC) of the array RT_interp
-    VTC = (RT_interp - np.mean(RT_interp)) / np.std(RT_interp)
-    if filt == True:
-        b, a = signal.butter(filt_order, filt_cutoff)
-        VTC_filtered = signal.filtfilt(b, a, abs(VTC))
-        VTC = VTC_filtered
+def compute_VTC(RT_array, subj_mean, subj_std):
+    """Computes the raw (unfiltered) VTC.
+
+    Parameters
+    ----------
+    RT_array : np.array
+        Array of reaction times after interpolation.
+    subj_mean : float
+        Mean reaction time of a subject across all runs.
+    subj_std : float
+        Standard deviation of reaction times of a subject across all runs.
+
+    Returns
+    -------
+    np.array
+        Array containing VTC values, should be the same length as the RT array.
+
+    """
+    return abs((RT_array - subj_mean) / subj_std)
+
+
+def clean_comerr(df_response):
+    cleaned_df = df_response.copy()
+    correct_omission_idx = []
+    commission_error_idx = []
+    correct_commission_idx = []
+    omission_error_idx = []
+    for idx_line, line in enumerate(cleaned_df.iterrows()):
+        if line[1][0] == 1.0 and line[1][1] != 0.0:  # Rare stim with response
+            cleaned_df.loc[idx_line, 4] == 0.0
+            commission_error_idx.append(idx_line)
+        if line[1][0] == 1.0 and line[1][1] == 0.0:  # Rare stim without response
+            correct_omission_idx.append(idx_line)
+        if line[1][0] == 2.0 and line[1][1] != 0.0:  # Freq stim with response
+            correct_commission_idx.append(idx_line)
+        if line[1][0] == 2.0 and line[1][1] == 0.0:  # Freq stim without response
+            omission_error_idx.append(idx_line)
+
+    performance_dict = {
+        "commission_error": commission_error_idx,
+        "correct_omission": correct_omission_idx,
+        "omission_error": omission_error_idx,
+        "correct_commission": correct_commission_idx,
+    }
+    return cleaned_df, performance_dict
+
+
+def threshold_VTC(VTC, thresh=5):
+    VTC[VTC >= thresh] = thresh
     return VTC
 
 
-def in_out_zone(VTC, lobound=None, hibound=None):
-    ### Collects the indices of IN/OUT zone trials
-    # lobound and hibound are values between 0 and 100 representing quantiles
-    INzone = []
-    OUTzone = []
-    if lobound == None and hibound == None:
-        VTC_med = np.median(VTC)
-        for i, val in enumerate(VTC):
-            if val < VTC_med:
-                INzone.append(i)
-            if val >= VTC_med:
-                OUTzone.append(i)
-    else:
-        low = np.quantile(VTC, lobound / 100)
-        high = np.quantile(VTC, hibound / 100)
-        for i, val in enumerate(VTC):
-            if val < low:
-                INzone.append(i)
-            if val >= high:
-                OUTzone.append(i)
-    INzone = np.asarray(INzone)
-    OUTzone = np.asarray(OUTzone)
-    return INzone, OUTzone
-
-
-def find_jumps(array):
-    ### Finds the jumps in an array containing ordered sequences
-    jumps = []
-    for i, _ in enumerate(array):
-        try:
-            if array[i + 1] != array[i] + 1:
-                jumps.append(i)
-        except:
-            break
-    return jumps
-
-
-def find_bounds(array):
-    ### Create a list of tuples, each containing the first and last values of every ordered sequences
-    # contained in a 1D array
-    jumps = find_jumps(array)
-    bounds = []
-    for i, jump in enumerate(jumps):
-        if jump == jumps[0]:
-            bounds.append(tuple([array[0], array[jump]]))
-        else:
-            bounds.append(tuple([array[jumps[i - 1] + 1], array[jump]]))
-        if i == len(jumps) - 1:
-            bounds.append(tuple([array[jump + 1], array[-1]]))
-    return bounds
-
-
-def old_get_VTC_from_file(
-    filepath, lobound=None, hibound=None, filt=True, filt_order=3, filt_cutoff=0.05
-):
-    data = loadmat(filepath)
-    df_response = pd.DataFrame(data["response"])
-    df_response = df_response[:-1]
-    RT_array = np.asarray(df_response.loc[:, 4])
-    RT_interp = interp_RT(RT_array)
-    VTC = compute_VTC(
-        RT_interp, filt=filt, filt_order=filt_order, filt_cutoff=filt_cutoff
-    )
-    INzone, OUTzone = in_out_zone(VTC, lobound=lobound, hibound=hibound)
-    INbounds = find_bounds(INzone)
-    OUTbounds = find_bounds(OUTzone)
-    return VTC, INbounds, OUTbounds, INzone, OUTzone, RT_array
-
-
 def get_VTC_from_file(
-    subject, run, files_list, cpt_blocs=[2, 3, 4, 5, 6, 7], inout_bounds=[25, 75]
+    subject,
+    run,
+    files_list,
+    cpt_blocs=[2, 3, 4, 5, 6, 7],
+    inout_bounds=[25, 75],
+    filt_cutoff=0.05,
 ):
     """Short summary.
 
@@ -178,7 +136,7 @@ def get_VTC_from_file(
     subject_logfiles = []
     for bloc in cpt_blocs:
         subject_logfiles.append(
-            op.join(LOGS_DIR, find_logfile(subject, run, files_list))
+            op.join(behav_path, find_logfile(subject, bloc, files_list))
         )
 
     # Load and clean RT arrays
@@ -187,15 +145,16 @@ def get_VTC_from_file(
     for idx_file, logfile in enumerate(subject_logfiles):
         data = loadmat(logfile)
         df_response = pd.DataFrame(data["response"])
+
         # Replace commission errors by 0
-        for idx_line, line in enumerate(df_response.iterrows()):
-            if line[1][0] == 1.0 and line[1][1] != 0.0:
-                df_response.loc[idx_line, 4] == 0.0
-        RT_raw = np.asarray(df_response.loc[:, 4])
+        df_clean, perf_dict = clean_comerr(df_response)
+        RT_raw = np.asarray(df_clean.loc[:, 4])
         RT_interpolated = interpolate_RT(RT_raw)
         RT_arrays.append(RT_interpolated)
         if int(cpt_blocs[idx_file]) == int(run):
             RT_to_VTC = RT_interpolated
+            performance_dict = perf_dict.copy()
+            df_response_out = df_response
 
     # Obtain meand and std across runs
     allruns_RT_array = np.concatenate(RT_arrays)
@@ -203,8 +162,10 @@ def get_VTC_from_file(
     subj_std = np.std(allruns_RT_array)
 
     # New VTC
-    VTC_raw = compute_VTC(RT_to_VTC, subj_mean, subj_std)
-    b, a = signal.butter(3, 0.05)  # (filt_order,filt_cutoff)
+    VTC_raw = threshold_VTC(
+        compute_VTC(RT_to_VTC, subj_mean, subj_std), thresh=3
+    )  # Compute VTC remove variability values above threshold
+    b, a = signal.butter(3, filt_cutoff)  # (filt_order,filt_cutoff)
     VTC_filtered = signal.filtfilt(b, a, VTC_raw)
 
     IN_mask = np.ma.masked_where(
@@ -216,7 +177,16 @@ def get_VTC_from_file(
     IN_idx = np.where(IN_mask.mask == False)[0]
     OUT_idx = np.where(OUT_mask.mask == False)[0]
 
-    return IN_idx, OUT_idx, VTC_raw, VTC_filtered, IN_mask, OUT_mask
+    return (
+        IN_idx,
+        OUT_idx,
+        VTC_raw,
+        VTC_filtered,
+        IN_mask,
+        OUT_mask,
+        performance_dict,
+        df_response_out,
+    )
 
 
 def plot_VTC(VTC_filtered, VTC_raw, IN_mask, OUT_mask, subject="?", bloc="?"):
