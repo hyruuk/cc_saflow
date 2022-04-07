f76b0fa645c5bdfe49a452c45f67c0440f0156a2
Hyruuk <yharel109@gmail.com>
2022-04-07 14:53:25 -0400

    rerouted multifeat to new script

--- slurm/7_classif_multifeat.sh
+++ slurm/7_classif_multifeat.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/classif_multifeat.py -m LR -c $1 -by VTC
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -mf 1 -m LR -c $1 -by VTC -s 50 50 -avg 0 
