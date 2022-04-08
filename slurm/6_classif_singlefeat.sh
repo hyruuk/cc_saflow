3f4918da2833afe0df11f8080cf05d2e7866ba8f
Hyruuk <yharel109@gmail.com>
2022-04-08 17:06:09 -0400

    adjusted alloc

--- slurm/6_classif_singlefeat.sh
+++ slurm/6_classif_singlefeat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-kjerbi
-#SBATCH --time=12:00:00
+#SBATCH --time=00:30:00
 #SBATCH --job-name=safSingleFeature
-#SBATCH --mem=8G
+#SBATCH --mem=2G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=4
 
 $HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -m LR -mf 0 -c $1 -by VTC -s 50 50 -avg 1
