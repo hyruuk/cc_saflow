b0107d5493ce10e68516bc6d33aa325fb25c696b
Hyruuk <yharel109@gmail.com>
2022-04-08 23:42:22 -0400

    added stage PSD

--- slurm/5_run_ttests.sh
+++ slurm/5_run_ttests.sh
@@ -7,4 +7,4 @@
 #SBATCH --ntasks-per-node=24
 
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_ttests.py -by $1 -s $2 $3 -cor $4 -avg $5
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_ttests.py -by $1 -s $2 $3 -cor $4 -avg $5 -stage $6
