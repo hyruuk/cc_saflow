81de69ec480075493a1f8e6f9beb34515b499410
Hyruuk <yharel109@gmail.com>
2022-04-11 16:47:11 -0400

    forced 1000 perms

--- slurm/7_classif_multifeat.sh
+++ slurm/7_classif_multifeat.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -mf 1 -m LR -f $1 -by VTC -s 50 50 -avg 0 -n 1
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -mf 1 -m LR -p 1000 -f $1 -by VTC -s 50 50 -avg 1 -n 1
