14b400fd366d64520862b6056b79059705fb74e8
Hyruuk <yharel109@gmail.com>
2022-04-15 01:19:13 -0400

    batch viz across runs

--- slurm/8batch_classif_topomaps.sh
+++ slurm/8batch_classif_topomaps.sh
@@ -1,3 +1,3 @@
 for RUN in 2 3 4 5 6 7 allruns; do
-  sbatch slurm/8_classif_topomaps.sh VTC_PSD_LDA_group-level_singlefeat_single-trial_normalized_1000perm_5050-split_run-$RUN
+  $HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/visualization/classif_topomaps.py -n VTC_PSD_LDA_group-level_singlefeat_single-trial_normalized_1000perm_5050-split_run-$RUN
 done
