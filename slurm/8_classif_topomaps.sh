83d0c5de7017a8c4fb2f60418a4a1f21993a5f94
Hyruuk <yharel109@gmail.com>
2022-04-18 13:27:52 -0400

    updated slurms

--- slurm/8_classif_topomaps.sh
+++ slurm/8_classif_topomaps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-kjerbi
-#SBATCH --time=12:00:00
+#SBATCH --time=1:00:00
 #SBATCH --job-name=safClassifTopomaps
 #SBATCH --mem=16G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/visualization/classif_topomaps.py
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/visualization/classif_topomaps.py -n $1
