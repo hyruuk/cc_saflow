84fdc524599c8f81318c76af452d9dd55588764e
Yann Harel <yharel109@gmail.com>
2022-04-18 22:37:09 -0400

    moved figs to notebook topomaps

--- saflow/models/run_ttests.py
+++ saflow/models/run_ttests.py
@@ -8,6 +8,7 @@ from saflow import (
     IMG_DIR,
 )
 import pickle
+import saflow
 from saflow.utils import array_topoplot, create_pval_mask, get_SAflow_bids
 import numpy as np
 from sklearn.model_selection import (
@@ -17,8 +18,7 @@ from sklearn.model_selection import (
     LeaveOneGroupOut,
     KFold,
 )
-from mlneurotools.ml import classification, StratifiedShuffleGroupSplit
-from mlneurotools.stats import ttest_perm
+from sklearn.preprocessing import StandardScaler
 import argparse
 import os
 import mne
@@ -27,15 +27,23 @@ from tqdm import tqdm
 import os.path as op
 from scipy import stats
 from statsmodels.stats.multitest import fdrcorrection
-
+from tqdm.autonotebook import tqdm
+from saflow.models.run_classifs import prepare_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-stage",
     "--stage",
-    default="PSD",
+    default="PSD4001200",
     type=str,
     help="PSD files to use (PSD or PSD4001200)",
+)
+parser.add_argument(
+    "-r",
+    "--run",
+    default="all",
+    type=str,
+    help="0 for all runs, all for all combinations",
 )
 parser.add_argument(
     "-f",
@@ -54,7 +62,7 @@ parser.add_argument(
 parser.add_argument(
     "-s",
     "--split",
-    default=[25, 75],
+    default=[50, 50],
     type=int,
     nargs="+",
     help="Bounds of percentile split",
@@ -83,7 +91,7 @@ parser.add_argument(
 parser.add_argument(
     "-avg",
     "--average",
-    default=0,
+    default=1,
     type=int,
     help="0 for no, 1 for yes",
 )
@@ -97,8 +105,6 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-# Change prepare_data to keep
-from saflow.models.run_classifs import prepare_data
 
 if __name__ == "__main__":
     split = args.split
@@ -115,156 +121,87 @@ if __name__ == "__main__":
         correction = None
     else:
         correction = args.correction
+    run = args.run
+    if run == "0":
+        run_name = "allruns"
+        BLOCS_LIST = [saflow.BLOCS_LIST]  # if run is 0, compute everything
+    elif run == "all":
+        run_name = ["allruns", "2", "3", "4", "5", "6", "7"]
+        BLOCS_LIST = [saflow.BLOCS_LIST, "2", "3", "4", "5", "6", "7"]
+    else:
+        BLOCS_LIST = [run]
+        run_name = [run]
 
-    if by == "VTC":
-        conds_list = (ZONE_CONDS[0] + str(split[0]), ZONE_CONDS[1] + str(split[1]))
-        savepath = op.join(
-            RESULTS_PATH,
-            f"VTC_ttest_{stage}_{n_perms}perm_{split[0]}{split[1]}_{correction}_avg{avg}/",
-        )
-        figpath = op.join(
-            IMG_DIR,
-            f"{by}_tvals_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{split[0]}{split[1]}_{correction}_avg{avg}.png",
-        )
-        figpath_contrast = op.join(
-            IMG_DIR,
-            f"{by}_contrast_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{split[0]}{split[1]}_{correction}_avg{avg}.png",
-        )
-        figpath_pvals = op.join(
-            IMG_DIR,
-            f"{by}_pvals_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{split[0]}{split[1]}_{correction}_avg{avg}.png",
-        )
-    elif by == "odd":
-        conds_list = ["FREQhits", "RAREhits"]
-        savepath = op.join(
-            RESULTS_PATH, f"odd_ttest_{stage}_{n_perms}perm_{correction}_avg{avg}/"
-        )
-        figpath = op.join(
-            IMG_DIR,
-            f"{by}_tvals_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{correction}_avg{avg}.png",
-        )
-        figpath_contrast = op.join(
-            IMG_DIR,
-            f"{by}_contrast_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{correction}_avg{avg}.png",
-        )
-        figpath_pvals = op.join(
-            IMG_DIR,
-            f"{by}_pvals_{stage}_{n_perms}perms_alpha{str(alpha)[2:]}_{correction}_avg{avg}.png",
-        )
+    for run_idx, RUNS in enumerate(BLOCS_LIST):
+        if by == "VTC":
+            conds_list = (ZONE_CONDS[0] + str(split[0]), ZONE_CONDS[1] + str(split[1]))
+            savepath = op.join(
+                RESULTS_PATH,
+                f"VTC_ttest_{stage}_{n_perms}perm_{split[0]}{split[1]}_{correction}_avg{avg}_run-{run_name[run_idx]}/",
+            )
 
-    if not (os.path.isdir(savepath)):
-        os.makedirs(savepath)
+        elif by == "odd":
+            conds_list = ["FREQhits", "RAREhits"]
+            savepath = op.join(
+                RESULTS_PATH,
+                f"odd_ttest_{stage}_{n_perms}perm_{correction}_avg{avg}_run-{run_name[run_idx]}/",
+            )
 
-    alltvals = []
-    allcontrasts = []
-    masks = []
-    allpvals = []
-    for FREQ in range(len(FREQS_NAMES)):
-        savename = "PSD_ttest_{}.pkl".format(FREQS_NAMES[FREQ])
-        condA_allchans = []
-        condB_allchans = []
-        X, y, groups = prepare_data(
-            BIDS_PATH,
-            SUBJ_LIST,
-            BLOCS_LIST,
-            conds_list,
-            stage=stage,
-            CHAN=[x for x in range(270)],
-            FREQ=FREQ,
-            balance=True,
-            avg=avg,
-            normalize=normalize,
-            level=level,
-        )
+        if not (os.path.isdir(savepath)):
+            os.makedirs(savepath)
 
-        if normalize:
+        alltvals = []
+        allcontrasts = []
+        masks = []
+        allpvals = []
+        for FREQ in range(len(FREQS_NAMES)):
+            savename = "PSD_ttest_{}.pkl".format(FREQS_NAMES[FREQ])
+            condA_allchans = []
+            condB_allchans = []
+            X, y, groups = prepare_data(
+                BIDS_PATH,
+                SUBJ_LIST,
+                RUNS,
+                conds_list,
+                stage=stage,
+                CHAN=[x for x in range(270)],
+                FREQ=FREQ,
+                balance=True,
+                avg=avg,
+                normalize=normalize,
+                level="group",
+            )
+            print(X[0, 0])
+            # if normalize:
             scaler = StandardScaler()
             X = scaler.fit_transform(X)
-        condA = [x for i, x in enumerate(X) if y[i] == 0]
-        condB = [x for i, x in enumerate(X) if y[i] == 1]
-        condA_allchans = np.asarray(condA)
-        condB_allchans = np.asarray(condB)
-        print(f"cond {conds_list[0]} shape : {condA_allchans.shape}")
-        print(f"cond {conds_list[1]} shape : {condB_allchans.shape}")
+            print(X[0, 0])
+            condA = [x for i, x in enumerate(X) if y[i] == 0]
+            condB = [x for i, x in enumerate(X) if y[i] == 1]
+            condA_allchans = np.asarray(condA)
+            condB_allchans = np.asarray(condB)
+            print(f"cond {conds_list[0]} shape : {condA_allchans.shape}")
+            print(f"cond {conds_list[1]} shape : {condB_allchans.shape}")
 
-        if not avg:
-            tvals, pvals = stats.ttest_ind(
-                condA_allchans,
-                condB_allchans,  # cond1 = IN, cond2 = OUT
-                permutations=n_perms + 1,
-            )
-        else:
-            tvals, pvals = stats.ttest_rel(condA_allchans, condB_allchans)
-        pvals = fdrcorrection(pvals, alpha=alpha)[1]
-        contrast = (condA_allchans - condB_allchans) / condB_allchans
-        contrast = np.mean(contrast, axis=0)
-        results = {"tvals": tvals, "pvals": pvals, "contrast": contrast}
+            if not avg:
+                tvals, pvals = stats.ttest_ind(
+                    condA_allchans,
+                    condB_allchans,  # cond1 = IN, cond2 = OUT
+                    permutations=n_perms + 1,
+                )
+            else:
+                tvals, pvals = stats.ttest_rel(condA_allchans, condB_allchans)
+            pvals = fdrcorrection(pvals, alpha=alpha)[1]
+            contrast = (condA_allchans - condB_allchans) / condB_allchans
+            contrast = np.mean(contrast, axis=0)
+            results = {"tvals": tvals, "pvals": pvals, "contrast": contrast}
 
-        with open(savepath + savename, "wb") as f:
-            pickle.dump(results, f)
-        print("Ok")
-        print(f"Min pval : {min(pvals)}")
+            with open(savepath + savename, "wb") as f:
+                pickle.dump(results, f)
+            print("Ok")
+            print(f"Min pval : {min(pvals)}")
 
-        allcontrasts.append(contrast)
-        alltvals.append(results["tvals"])
-        allpvals.append(results["pvals"])
-        masks.append(create_pval_mask(results["pvals"], alpha=alpha))
-
-    # Plots
-    # obtain chan locations
-    _, data_fname = get_SAflow_bids(BIDS_PATH, subj="04", run="2", stage="-epo")
-    epochs = mne.read_epochs(data_fname)
-    ch_xy = epochs.pick_types(
-        meg=True, ref_meg=False
-    ).info  # Find the channel positions
-
-    # Plot tvals
-    toplot = alltvals
-    vmax = np.max(np.max(abs(np.asarray(toplot))))
-    vmin = -vmax
-    array_topoplot(
-        toplot,
-        ch_xy,
-        showtitle=True,
-        titles=FREQS_NAMES,
-        savefig=True,
-        figpath=figpath,
-        vmin=vmin,
-        vmax=vmax,
-        with_mask=True,
-        masks=masks,
-        cmap="magma",
-    )
-
-    # Plot contrasts
-    toplot = allcontrasts
-    vmax = np.max(np.max(abs(np.asarray(toplot))))
-    vmin = -vmax
-    array_topoplot(
-        toplot,
-        ch_xy,
-        showtitle=True,
-        titles=FREQS_NAMES,
-        savefig=True,
-        figpath=figpath_contrast,
-        vmin=vmin,
-        vmax=vmax,
-        with_mask=True,
-        masks=masks,
-        cmap="coolwarm",
-    )
-
-    toplot = allpvals
-    array_topoplot(
-        toplot,
-        ch_xy,
-        showtitle=True,
-        titles=FREQS_NAMES,
-        savefig=True,
-        figpath=figpath_pvals,
-        vmin=0,
-        vmax=1,
-        with_mask=True,
-        masks=masks,
-        cmap="cividis",
-    )
+            allcontrasts.append(contrast)
+            alltvals.append(results["tvals"])
+            allpvals.append(results["pvals"])
+            masks.append(create_pval_mask(results["pvals"], alpha=alpha))
