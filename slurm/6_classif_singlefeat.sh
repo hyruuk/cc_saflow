93aecb75a452259b81e78c8b09499481402a55e3
Yann Harel <yharel109@gmail.com>
2022-04-20 20:13:44 -0400

    adjusted batch classif

--- slurm/6_classif_singlefeat.sh
+++ slurm/6_classif_singlefeat.sh
@@ -6,4 +6,4 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -m LDA -mf 0 -stage PSD4001200 -by $1 -s $2 $3 -avg 0 -n 1 -r all -l $4 -subj $5
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_classifs.py -m LDA -mf 0 -stage PSD4001200 -by $1 -s $2 $3 -avg 0 -n 1 -l $4 -subj $5 -r $6
