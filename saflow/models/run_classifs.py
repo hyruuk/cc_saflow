77db51a2563184cb7fcc38f4435857d96a44de2a
Hyruuk <yharel109@gmail.com>
2022-04-21 17:03:24 -0400

    changed DA for auc

--- saflow/models/run_classifs.py
+++ saflow/models/run_classifs.py
@@ -335,7 +335,14 @@ def classif_LOGO(X, y, groups, n_cvgroups, n_perms, model, avg=0, norm=1):
 
 def final_classif(pipeline, cv, X, y, groups, model, norm, n_perms=1000):
     score, permutation_scores, pvalue = permutation_test_score(
-        pipeline, X, y, groups=groups, cv=cv, n_permutations=n_perms, n_jobs=-1
+        pipeline,
+        X,
+        y,
+        groups=groups,
+        cv=cv,
+        n_permutations=n_perms,
+        n_jobs=-1,
+        scoring="auc",
     )
     results = {
         "acc_score": score,
