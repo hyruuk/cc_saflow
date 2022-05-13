cef3f1f34bb11fce5970b98c18bf47ed183855a9
Hyruuk <yharel109@gmail.com>
2022-05-13 05:20:18 +0200

    updated segmentation batch

--- saflow/data/segmentation.sh
+++ saflow/data/segmentation.sh
@@ -1,3 +1,3 @@
-for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25; do
+for SUB in 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 28 29 30 31 32 33 34 35 36 37 38; do
   python saflow/data/segmentation.py -s $SUB
 done
