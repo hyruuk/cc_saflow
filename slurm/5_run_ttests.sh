da1307dad5e3bd1c033f601026fa7ca62b0a5288
Hyruuk <yharel109@gmail.com>
2022-05-17 19:18:04 +0200

    adjusted ttests alloc

--- slurm/5_run_ttests.sh
+++ slurm/5_run_ttests.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=def-kjerbi
 #SBATCH --time=12:00:00
 #SBATCH --job-name=safRunTTests
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 
 
-$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_ttests.py -by $1 -s $2 $3 -avg $5 -stage $6
+$HOME/python_envs/cc_saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/models/run_ttests.py -by $1 -s $2 $3 -avg $4 -stage $5
