0be68c8a0064d064dcacc87f56c6e7a9d53ecd53
Hyruuk <yharel109@gmail.com>
2022-05-17 14:19:23 +0200

    updated ttest batches

--- slurm/5batch_run_ttests.sh
+++ slurm/5batch_run_ttests.sh
@@ -1,62 +1,62 @@
 
-sbatch ./slurm/5_run_ttests.sh VTC 50 50 'fdr' 0 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 50 50 0 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 25 75 'fdr' 0 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 25 75 0 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 10 90 'fdr' 0 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 10 90 0 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh odd 10 90 'fdr' 0 PSD
+sbatch ./slurm/5_run_ttests.sh odd 10 90 0 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 50 50 'fdr' 1 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 50 50 1 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 25 75 'fdr' 1 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 25 75 1 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 10 90 'fdr' 1 PSD
+sbatch ./slurm/5_run_ttests.sh VTC 10 90 1 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh odd 10 90 'fdr' 1 PSD
+sbatch ./slurm/5_run_ttests.sh odd 10 90 1 PSD
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 50 50 'fdr' 0 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 50 50 0 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 25 75 'fdr' 0 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 25 75 0 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 10 90 'fdr' 0 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 10 90 0 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh odd 10 90 'fdr' 0 PSD4001200
+sbatch ./slurm/5_run_ttests.sh odd 10 90 0 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 50 50 'fdr' 1 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 50 50 1 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 25 75 'fdr' 1 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 25 75  1 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh VTC 10 90 'fdr' 1 PSD4001200
+sbatch ./slurm/5_run_ttests.sh VTC 10 90  1 PSD4001200
 
 
 
-sbatch ./slurm/5_run_ttests.sh odd 10 90 'fdr' 1 PSD4001200
+sbatch ./slurm/5_run_ttests.sh odd 10 90  1 PSD4001200
