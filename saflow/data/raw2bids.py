68ecfa2537d322c19ca2e0f5d633a01dda1057b7
Yann Harel <yharel109@gmail.com>
2023-07-04 01:03:39 -0400

    read events

--- saflow/data/raw2bids.py
+++ saflow/data/raw2bids.py
@@ -2,12 +2,121 @@ import mne
 from mne_bids import write_raw_bids, BIDSPath
 from heudiconv.cli.run import main as heudiconv_main
 import scipy.io as sio
+import os.path as op
 import os
 import glob
 import json
+import argparse
+from datetime import datetime, timezone
 
+import mne
+from mne.datasets import sample
+
+from mne_bids import (write_raw_bids, read_raw_bids,
+                      BIDSPath, print_dir_tree)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-i",
+    "--input",
+    default="/media/hyruuk/CoCoLabYANN/coco_data/saflow/raw",#'./data/raw',
+    type=str,
+    help="Path to the raw data folder",
+)
+parser.add_argument(
+	"-o",
+	"--output",
+	default = "/media/hyruuk/CoCoLabYANN/coco_data/saflow/bids",#"./data/bids",
+	type = str,
+	help="Path to the output BIDS folder"
+)
+
+# Define constants
+EVENT_ID = {"Freq": 21, "Rare": 31, "Response": 99, "BlocStart": 10}
+
+def parse_info_from_name(fname):
+    # Parse info from a recording file
+    subject = fname.split("SA")[1][:2]
+    run = fname.split("_")[-1][:2]
+    if run in ["01", "07"]:
+        task = "rest"
+    else:
+        task = "gradCPT"
+    return subject, run, task
+
+def write_noise_file(ds_file, bids_root):
+    er_raw = mne.io.read_raw_ctf(ds_file)
+    er_raw.info['line_freq'] = 60
+    er_date = er_raw.info['meas_date'].strftime('%Y%m%d')
+    er_bids_path = BIDSPath(subject='emptyroom', 
+                            session=er_date,
+                            task='noise', 
+                            datatype="meg",
+                            extension=".fif",
+                            root=bids_root)
+    write_raw_bids(er_raw, er_bids_path, overwrite=False)
+
+def load_recording(fname, bids_root):
+    subject, run, task = parse_info_from_name(fname)
+    bidspath = BIDSPath(subject=subject,
+                        task=task,
+                        run=run,
+                        datatype="meg",
+                        extension=".fif",
+                        root=bids_root)
+    raw = mne.io.read_raw_ctf(ds_file)
+    raw.info['line_freq'] = 60
+    return raw, bidspath, task
+
+def events_from_file(raw):
+    events_mne, event_id = mne.events_from_annotations(raw)
+    event_id_inverted = {v: k for k, v in event_id.items()}
+    sampling_rate = raw.info['sfreq']
+    events_bids = {'onset': events_mne[:, 0] / sampling_rate,
+                   'duration': events_mne[:, 1] / sampling_rate,
+                   'trial_type': [event_id_inverted[i] for i in events_mne[:, 2]]}
+    return events_mne, events_bids, event_id
+
+if __name__ == "__main__":
+    # Parse arguments
+    args = parser.parse_args()
+    raw_folder = args.input
+    bids_root = args.output
+    os.makedirs(bids_root, exist_ok=True)
+    # List MEG files
+    meg_folder = op.join(raw_folder, "meg")
+    ds_folders = os.listdir(meg_folder)
+
+    ds_files = glob.glob(op.join(meg_folder, "*", "*.ds"))
+    for ds_file in sorted(ds_files):
+        fname = ds_file.split("/")[-1]
+        # Handle noise files
+        if "NOISE1Trial5min" in fname:
+            write_noise_file(ds_file, bids_root)
+        # Then recording files
+        elif "SA" in fname and not "procedure" in fname:
+            raw, bidspath, task = load_recording(fname, bids_root)
+            if task == "gradCPT":
+                events_mne, events_bids, event_id = events_from_file(raw)
+                0/0
+                write_raw_bids(
+                    raw,
+                    bidspath,
+                    events_data=events,
+                    event_id=event_id,
+                    overwrite=True,
+                )
+                
+            else:
+                write_raw_bids(raw, bidspath, overwrite=False)
+
+            
+
+
+
+'''
 # MEG
-ds_folders = glob.glob('/path/to/your/data/*')  # adjust this to your needs
+ds_folders = glob.glob(op.join(raw_folder, "meg"))  # adjust this to your needs
 
 for ds_folder in ds_folders:
     ds_files = glob.glob(os.path.join(ds_folder, '*.ds'))
@@ -58,3 +167,5 @@ for mat_file in mat_files:
                              suffix='events',
                              extension='.tsv')
         events.to_csv(bids_path, sep='\t', index=False)
+'''
+
