b322b40522948f96a2c9edfba6c6f37a405285d6
Yann Harel <yharel109@gmail.com>
2023-08-24 10:04:50 -0400

    updated preproc path and gitignore

--- saflow/data/preprocessing.py
+++ saflow/data/preprocessing.py
@@ -1,9 +1,10 @@
 import os
 import os.path as op
-from saflow.utils import get_SAflow_bids
+#from saflow.utils import get_SAflow_bids
 from saflow.neuro import saflow_preproc
 from saflow import BIDS_PATH, SUBJ_LIST, BLOCS_LIST
 import argparse
+from mne_bids import BIDSPath
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -30,4 +31,24 @@ if __name__ == "__main__":
 		file_path = get_SAflow_bids(BIDS_PATH, subj=subj, run=bloc, stage='raw')[1]
 		save_path = get_SAflow_bids(BIDS_PATH, subj=subj, run=bloc, stage='preproc_raw')[1]
 		report_path = get_SAflow_bids(BIDS_PATH, subj=subj, run=bloc, stage='preproc_report')[1]
-		saflow_preproc(file_path, save_path, report_path, ica=ica)
+
+		input_path = BIDSPath(subject=subj,
+					task="gradCPT",
+					run='0'+bloc,
+					datatype="meg",
+					extension=".fif",
+					root=BIDS_PATH)
+		output_path = BIDSPath(subject=subj,
+			task="gradCPT",
+			run='0'+bloc,
+			datatype="meg",
+			description='cleaned',
+			extension=".fif",
+			root=BIDS_PATH)
+		report_path = BIDSPath(subject=subj,
+			task="gradCPT",
+			run='0'+bloc,
+			datatype="report",
+			extension=".html",
+			root=BIDS_PATH)
+		saflow_preproc(input_path, output_path, report_path, ica=ica)
