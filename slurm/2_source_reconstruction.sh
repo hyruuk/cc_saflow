28665e32f1cc28695db30c60975b940d009be012
hyruuk <yharel109@gmail.com>
2023-09-23 11:35:21 -0400

    adjusted slurmfile

--- slurm/2_source_reconstruction.sh
+++ slurm/2_source_reconstruction.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --account=def-kjerbi
-#SBATCH --time=12:00:00
-#SBATCH --job-name=safSegmentation
-#SBATCH --mem=128G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
-
+#SBATCH --account=def-pbellec
+#SBATCH --time=2:00:00
+#SBATCH --job-name=safInverse
+#SBATCH --mem=256G
+#SBATCH --ntasks=1
 
 $HOME/python_envs/saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/source_reconstruction/inverse_solution.py -s $1 -r $2
