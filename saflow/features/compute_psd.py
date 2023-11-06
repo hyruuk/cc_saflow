5f3d24ad028be998e9a412cf6d5cab56700752ea
Yann Harel <yharel109@gmail.com>
2023-11-06 04:25:52 -0500

    added level and method

--- saflow/features/compute_psd.py
+++ saflow/features/compute_psd.py
@@ -13,6 +13,7 @@ from mne_bids import BIDSPath, read_raw_bids
 #from neurokit2.complexity import complexity_lempelziv, complexity_dimension, complexity_delay
 import pickle
 from saflow.features.utils import create_fnames, segment_sourcelevel
+import os.path as op
 
 parser = argparse.ArgumentParser()
 
@@ -24,22 +25,36 @@ parser.add_argument(
     help="Number of trials to consider per epoch",
 )
 parser.add_argument(
+    "-l",
+    "--level",
+    default="sensor",
+    type=str,
+    help="Level of processing (sensor or source)",
+)
+parser.add_argument(
+    "-m",
+    "--method",
+    default="hilbert",
+    type=str,
+    help="Level of processing (sensor or source)",
+)
+parser.add_argument(
     "-s",
     "--subject",
-    default='12',
+    default='all',
     type=str,
     help="Subject to process",
 )
 parser.add_argument(
     "-r",
     "--run",
-    default='02',
+    default='all',
     type=str,
     help="Run to process",
 )
 
 
-def compute_hilbert_env(stc, l_freq, h_freq):
+def compute_hilbert_env(data, sfreq, l_freq, h_freq):
     """
     Compute the Hilbert envelope of a source estimate object, after band-pass filtering it between l_freq and h_freq.
 
@@ -57,8 +72,8 @@ def compute_hilbert_env(stc, l_freq, h_freq):
     stc_hilb : np.ndarray
         The Hilbert envelope of the source estimate object, with shape (n_vertices, n_samples).
     """
-    stc_filtered = mne.filter.filter_data(np.float64(stc.data), 
-                                          sfreq=stc.sfreq, 
+    stc_filtered = mne.filter.filter_data(data, 
+                                          sfreq=sfreq, 
                                           l_freq=l_freq, 
                                           h_freq=h_freq, 
                                           n_jobs=-1)
@@ -87,7 +102,7 @@ def time_average(segmented_array):
     time_avg_array = np.array(time_avg_array)
     return time_avg_array
 
-def compute_PSD(stc, filepaths, n_trials=1):
+def compute_PSD(data, filepaths, sfreq, n_trials=1, method='welch'):
     """
     Compute the power spectral density (PSD) of a source estimate object across different frequency bands.
 
@@ -101,14 +116,31 @@ def compute_PSD(stc, filepaths, n_trials=1):
     psd_array : np.ndarray
         The PSD of the source estimate object, with shape (n_freq_bands, n_events, n_channels).
     """
-    psd_array = []
-    for idx, freq in enumerate(saflow.FREQS_NAMES):
-        stc_env = compute_hilbert_env(stc, saflow.FREQS[idx][0], saflow.FREQS[idx][1])
-        segmented_array, events_idx, events_dicts = segment_sourcelevel(stc_env, filepaths, sfreq=stc.sfreq, n_events_window=n_trials)
-        time_avg_array = time_average(segmented_array)
-        psd_array.append(time_avg_array)
-    psd_array = np.array(psd_array).transpose(1,0,2)
+    if method == 'hilbert':
+        psd_array = []
+        for idx, freq in enumerate(saflow.FREQS_NAMES):
+            stc_env = compute_hilbert_env(data, sfreq, saflow.FREQS[idx][0], saflow.FREQS[idx][1])
+            segmented_array, events_idx, events_dicts = segment_sourcelevel(stc_env, filepaths, sfreq=sfreq, n_events_window=n_trials)
+            time_avg_array = time_average(segmented_array)
+            psd_array.append(time_avg_array)
+        psd_array = np.array(psd_array).transpose(1,0,2)
+    elif method == 'welch':
+        psd_array = []
+        segmented_array, events_idx, events_dicts = segment_sourcelevel(data, filepaths, sfreq=sfreq, n_events_window=n_trials)
+        welch_array = mne.time_frequency.psd_array_welch(segmented_array, sfreq=sfreq, n_jobs=-1, n_fft=1022)#fmin=saflow.FREQS[idx][0], fmax=saflow.FREQS[idx][1])
+        for idx, freq in enumerate(saflow.FREQS_NAMES):
 
+            lowfreq = saflow.FREQS[idx][0]
+            highfreq = saflow.FREQS[idx][1]
+            freq_mask = np.where((welch_array[1] >= lowfreq) & (welch_array[1] <= highfreq))[0]
+
+            if len(freq_mask) <= 1:
+                welch_array_masked = welch_array[0][:,:,freq_mask]
+            else:
+                welch_array_masked = welch_array[0][:,:,freq_mask[0]:freq_mask[-1]+1]
+            psd_array.append(np.mean(welch_array_masked, axis=-1))
+        psd_array = np.array(psd_array).transpose(1,0,2)
+        
     return psd_array, events_idx, events_dicts
 
 
@@ -117,21 +149,46 @@ if __name__ == "__main__":
     n_trials = args.n_trials
     subject = args.subject
     run = args.run
+    level = args.level
+    method = args.method
     freq_bands = saflow.FREQS
     freq_names = saflow.FREQS_NAMES
 
-    filepaths = create_fnames(subject, run)
-    if n_trials > 1:
-        filepaths['psd'].update(root=saflow.BIDS_PATH + f'/derivatives/psd_{n_trials}/')
-        filepaths['psd'].mkdir(exist_ok=True)
+    if type(run) == str:
+        if run == 'all':
+            runs = ['02', '03', '04', '05', '06', '07']
+        else:
+            runs = [run]
+    elif type(run) == list:
+        runs = run
 
-    stc = mne.read_source_estimate(filepaths['morph'])
+    if type(subject) == str:
+        if subject == 'all':
+            subjects = saflow.SUBJ_LIST
+        else:
+            subjects = [subject]
 
-    psd_array, events_idx, events_dicts = compute_PSD(stc, filepaths, n_trials=1)
+    for subject in subjects:
+        for run in runs:
+            filepaths = create_fnames(subject, run)
+            filepaths['psd'].update(root=op.join('/'.join(str(filepaths['psd'].root).split('/')[:-1]), str(filepaths['psd'].root).split('/')[-1] + f'_{method}_{level}_{n_trials}'))
 
-    for idx, array in enumerate(psd_array):
-        fname = str(filepaths['psd'].fpath).replace('idx', str(events_idx[idx])) + '.pkl'
-        with open(fname, 'wb') as f:
-            pickle.dump({'data':array,
-                         'info':events_dicts[idx]}, 
-                         f)
+            filepaths['psd'].mkdir(exist_ok=True)
+
+            if level == 'source':
+                stc = mne.read_source_estimate(filepaths['morph'])
+                data = np.float64(stc.data)
+                sfreq = stc.sfreq
+            elif level == 'sensor':
+                raw = mne_bids.read_raw_bids(filepaths['preproc'])
+                data = raw.get_data()
+                sfreq = raw.info['sfreq']
+
+            psd_array, events_idx, events_dicts = compute_PSD(data, filepaths, sfreq, n_trials=n_trials, method=method)
+
+            for idx, array in enumerate(psd_array):
+                fname = str(filepaths['psd'].fpath).replace('idx', str(events_idx[idx])) + '.pkl'
+                with open(fname, 'wb') as f:
+                    pickle.dump({'data':array,
+                                'info':events_dicts[idx]}, 
+                                f)
