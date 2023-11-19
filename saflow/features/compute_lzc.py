78326d70cc1965ec6dab8d797cb854508d4ee57f
Yann Harel <yharel109@gmail.com>
2023-11-18 22:56:25 -0500

    revert to antropy

--- saflow/features/compute_lzc.py
+++ saflow/features/compute_lzc.py
@@ -17,7 +17,7 @@ import numpy as np
 import pickle
 from saflow.features.utils import create_fnames, segment_sourcelevel
 import os.path as op
-#import antropy
+import antropy
 
 
 parser = argparse.ArgumentParser()
@@ -67,17 +67,18 @@ def compute_lzc_for_epoch(epoch, idx, events_dict, filepaths):
     else:
         with open(fname, 'rb') as f:
             file = pickle.load(f)
-        epoch_array = file['data']
+            epoch_array = file['data']
+
     return epoch_array
 
 def compute_lzc_for_chan(channel, chan_idx):
     # Compute LZC and permuted LZC
     #plzc = complexity_lempelziv(channel, permutation=True, dimension=7, delay=2)[0]
-    nk_lzc = complexity_lempelziv(channel, symbolize='median', permutation=False)[0]
-    #channel_binarized = np.array([0 if x < np.median(channel) else 1 for x in channel])
-    #ant_lzc = antropy.lziv_complexity(channel_binarized, normalize=True)
-    #print(f'Chan : {chan_idx}, LZC = {lzc}')
-    return [nk_lzc, 0]#plzc]
+    #nk_lzc = complexity_lempelziv(channel, symbolize='median', permutation=False)[0]
+    channel_binarized = np.array([0 if x < np.median(channel) else 1 for x in channel])
+    ant_lzc = antropy.lziv_complexity(channel_binarized, normalize=True)
+    print(f'Chan : {chan_idx}, LZC = {ant_lzc}')
+    return [ant_lzc, 0]#plzc]
 
 
 def compute_LZC_on_sources(data, sfreq, filepaths, n_trials=8):
@@ -85,7 +86,6 @@ def compute_LZC_on_sources(data, sfreq, filepaths, n_trials=8):
     segmented_array, events_idx, events_dicts = segment_sourcelevel(data, filepaths, sfreq=sfreq, n_events_window=n_trials)
     
     for epo_idx, epoch in enumerate(segmented_array):
-        print(f'Epoch {epo_idx}')
         lzc_array = compute_lzc_for_epoch(epoch, epo_idx, events_dicts[epo_idx], filepaths)
     
     lzc_array = np.array(lzc_array)
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         for run in runs:
             print(f'Processing subject {subject}, run {run}')
             filepaths = create_fnames(subject, run)
-            filepaths['lzc'].update(root=op.join('/'.join(str(filepaths['lzc'].root).split('/')[:-1]), str(filepaths['lzc'].root).split('/')[-1] + f'_{level}_{n_trials}trials_noplzc_nk'))
+            filepaths['lzc'].update(root=op.join('/'.join(str(filepaths['lzc'].root).split('/')[:-1]), str(filepaths['lzc'].root).split('/')[-1] + f'_{level}_{n_trials}trials_noplzc_ant'))
             filepaths['lzc'].mkdir(exist_ok=True)
             if level == 'sensor':
                 raw = mne_bids.read_raw_bids(filepaths['preproc'])
