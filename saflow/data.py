bd94947fb8dc4d6c623cc363d4f694df5b4ab589
Yann Harel <yharel109@gmail.com>
2023-11-28 16:43:52 -0500

    updated modules

---
+++ saflow/data.py
@@ -0,0 +1,217 @@
+import saflow
+from saflow.stats import subject_average, simple_contrast, subject_contrast
+import numpy as np
+import os.path as op
+import pickle
+import pickle as pkl
+import random
+import os
+from saflow.neuro import average_bands
+
+def load_fooof_data(feature, feature_fpath, feat_to_get, trial_type_to_get, classif='INOUT_2575'):
+    if type(trial_type_to_get) == str:
+        trial_type_to_get = [trial_type_to_get]
+    
+    X = []
+    y = []
+    groups = []
+    VTC = []
+    task = []
+    for sub in sorted(os.listdir(feature_fpath)):
+        for file in sorted(os.listdir(op.join(feature_fpath, sub, 'meg'))):
+            print(file)
+            if 'magic' in file:
+                fname = op.join(feature_fpath, sub, 'meg', file)
+                fname_output = fname.replace('.pkl', '_magic.pkl')
+                with open(fname, 'rb') as f:
+                    data = pickle.load(f)
+                # Reshape data
+                n_chans = 270
+                n_trials = int(len(data)/n_chans)
+                data_reshaped = np.array(data).reshape(n_trials, n_chans)
+
+                for trial_idx in range(data_reshaped.shape[0]):
+                    epoch_selected = select_trial(data_reshaped[trial_idx][0]['info'], trial_type=trial_type_to_get)
+                    if epoch_selected:
+                        trial_data = get_trial_data(data_reshaped, trial_idx, feat_to_get)
+                        if not np.isnan(trial_data).any():
+                            X.append(trial_data)
+                            groups.append(sub)
+                            VTC.append(np.nanmean(data_reshaped[trial_idx][0]['info']['included_VTC']))
+                            task.append(data_reshaped[trial_idx][0]['info']['task'])
+                            if 'INOUT' in classif:
+                                # 0 = IN, 1 = OUT
+                                if data_reshaped[trial_idx][0]['info'][classif] == 'IN':
+                                    y.append(0)
+                                elif data_reshaped[trial_idx][0]['info'][classif] == 'OUT':
+                                    y.append(1)
+                            elif classif == 'oddball':
+                                # 0 = rare, 1 = frequent
+                                if data_reshaped[trial_idx][0]['info']['task'] == 'correct_omission':
+                                    y.append(0)
+                                elif data_reshaped[trial_idx][0]['info']['task'] == 'correct_commission':
+                                    y.append(1)
+                        else:
+                            print('Nan in trial data')
+                            print(f'{sub} {file}, trial {trial_idx}')
+    X = np.array(X).transpose(2,0,1)
+    y = np.array(y)
+    groups = np.array(groups)
+    VTC = np.array(VTC)
+    task = np.array(task)
+    return X, y, groups, VTC, task
+
+def load_features(feature_folder, subject='all', feature='psd', splitby='inout', inout='INOUT_2575', remove_errors=True, get_task = ['correct_commission']):
+    ''' 0 = IN, 1 = OUT
+    '''
+    subject
+    X = []
+    y = []
+    groups = []
+    VTC = []
+    task = []
+    for idx_subj, subj in enumerate(sorted(os.listdir(feature_folder))):
+        for trial in os.listdir(op.join(feature_folder, subj, 'meg')):
+            if 'desc-' in trial:
+                filepath = op.join(feature_folder, subj, 'meg', trial)
+                with open(filepath, 'rb') as f:
+                    data = pkl.load(f)
+                print(trial)
+                epoch_selected = select_trial(data['info'], trial_type=get_task)
+                if epoch_selected:
+                    try:
+                        if splitby == 'inout':
+                            if type(data['info'][inout]) == str:
+                                if data['info'][inout] == 'IN':
+                                    y.append(0)
+                                elif data['info'][inout] == 'OUT':
+                                    y.append(1)
+                                groups.append(idx_subj)
+
+                                if feature in ['psd', 'lzc']:
+                                    X.append(data['data'])
+
+                                elif feature in ['slope', 'offset', 'r_squared', 'knee']:
+                                    with open(filepath.replace('_desc-', '_fg-'), 'rb') as f:
+                                        fooof_data = pkl.load(f)
+                                    temp_X = []
+                                    for fm in fooof_data['fooof']:
+                                        if feature == 'slope':
+                                            temp_X.append(fm.get_params('aperiodic_params')[-1])
+                                        elif feature == 'offset':
+                                            temp_X.append(fm.get_params('aperiodic_params')[0])
+                                        elif feature == 'knee':
+                                            temp_X.append(fm.get_params('aperiodic_params')[1])
+                                        elif feature == 'r_squared':
+                                            temp_X.append(fm.get_params('r_squared'))
+                                    X.append(np.array(temp_X))
+
+                                VTC.append(np.nanmean(data['info']['included_VTC']))
+                                task.append(data['info']['task'])
+                    except Exception as e:
+                        print(f'Could not load {trial}')
+                        print(e)
+    X = np.array(X)
+    if X.ndim == 3:
+        X = X.transpose(1,0,2)
+    y = np.array(y)
+    groups = np.array(groups)
+    VTC = np.array(VTC)
+    task = np.array(task)
+    return X, y, groups, {'vtc':VTC, 'task':task}
+
+def get_trial_data(data_reshaped, trial_idx, feat_to_get, freq_bins=None):
+    trial_data = []
+    for chan_idx in range(data_reshaped.shape[1]):
+        if feat_to_get == 'ksor':
+            feat_data = []
+            feat_data.append(data_reshaped[trial_idx][chan_idx]['knee'])
+            feat_data.append(data_reshaped[trial_idx][chan_idx]['exponent'])
+            feat_data.append(data_reshaped[trial_idx][chan_idx]['offset'])
+            feat_data.append(data_reshaped[trial_idx][chan_idx]['r_squared'])
+            trial_data.append(np.array(feat_data))
+        else:
+            psd = data_reshaped[trial_idx][chan_idx][feat_to_get]
+            if freq_bins is None:
+                freq_bins = data_reshaped[trial_idx][chan_idx]['freq_bins']
+            power_bands = average_bands(psd, freq_bins)
+            trial_data.append(power_bands)
+    return np.array(trial_data)
+
+
+def balance_data(X, y, groups, seed=10):
+    X_balanced = []
+    y_balanced = []
+    groups_balanced = []
+    # We want to balance the trials across subjects
+    random.seed(seed)
+    X = X.transpose(1,0,2)
+    for subj_idx in np.unique(groups):
+        y_subj = [label for i, label in enumerate(y) if groups[i] == subj_idx]
+        if len(np.unique(y_subj)) > 1:
+            max_trials = min(np.unique(y_subj, return_counts=True)[1])
+        
+            X_subj_0 = [x for i, x in enumerate(X) if groups[i] == subj_idx and y[i] == 0]
+            X_subj_1 = [x for i, x in enumerate(X) if groups[i] == subj_idx and y[i] == 1]
+
+            idx_list_0 = [x for x in range(len(X_subj_0))]
+            idx_list_1 = [x for x in range(len(X_subj_1))]
+            inout = 'IN' if y_subj[0] == 0 else 'OUT'
+            print(f'Subject {subj_idx} max trials : {max_trials} {inout}')
+            print(len(idx_list_0), len(idx_list_1))
+            print(max_trials)
+            picks_0 = random.sample(idx_list_0, max_trials)
+            picks_1 = random.sample(idx_list_1, max_trials)
+
+            for i in range(max_trials):
+                X_balanced.append(X_subj_0[picks_0[i]])
+                y_balanced.append(0)
+                groups_balanced.append(subj_idx)
+                X_balanced.append(X_subj_1[picks_1[i]])
+                y_balanced.append(1)
+                groups_balanced.append(subj_idx)
+        else:
+            print(f'Not enough trials in one condition. Subject {subj_idx} removed.')
+    X = np.array(X_balanced).transpose(1,0,2)
+    y = np.array(y_balanced)
+    groups = np.array(groups_balanced)
+    return X, y, groups
+
+
+def select_trial(event_dict, trial_type=['correct_commission'], type_how='all', bad_how='any', inout_how='all', inout='INOUT_2575', verbose=False):
+    if bad_how is None or bad_how == 'ignore':
+        bad_epoch = False
+    elif bad_how == 'last':
+        bad_epoch = event_dict['bad_epoch']
+    elif bad_how == 'any':
+        bad_epoch = np.sum(event_dict['included_bad_epochs']) > 0
+
+    if str(event_dict[inout]) != 'nan':
+        if inout_how == 'all':
+            n_uniques = len(np.unique(event_dict['included_INOUT'])) == 1
+            unique_elements = np.unique(event_dict['included_INOUT'])
+            uniques_inout = any(element in ['IN', 'OUT'] for element in unique_elements)
+            retain_inout =  n_uniques & uniques_inout
+        elif inout_how == 'last':
+            retain_inout = event_dict[inout] in ['IN', 'OUT']
+    else:
+        retain_inout = False
+
+    if type_how == 'last':
+        retain_type = event_dict['task'] in trial_type
+    if type_how == 'all':
+        if len(np.unique(event_dict['included_task'])) == 1:
+            if np.unique(event_dict['included_task']) == 'correct_omission':
+                retain_type = True
+            else:
+                retain_type = False
+        else:
+            if event_dict['task'] in trial_type:
+                retain_type = True
+            else:
+                retain_type = False
+    
+    retain_epoch = retain_inout & retain_type & ~bad_epoch
+    if verbose:
+        print(f'Bad : {bad_epoch}, InOut : {retain_inout}, Type : {retain_type}, Retain : {retain_epoch}')
+    return retain_epoch
---
+++ saflow/stats.py
@@ -0,0 +1,118 @@
+import numpy as np
+import mne
+from mne_bids import BIDSPath, read_raw_bids
+from fooof import FOOOF, Bands, FOOOFGroup
+import mne_bids
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.model_selection import permutation_test_score
+from scipy import stats
+
+
+def singlefeat_classif(clf, cv, X, y, groups, n_perms=1):
+    clf = LinearDiscriminantAnalysis()
+    cv = LeaveOneGroupOut()
+
+    all_scores, all_perm_scores, all_pvals = [], [], []
+    for freq_idx in range(X.shape[0]):
+        scores, perm_scores, pvals = [], [], []
+        for chan_idx in range(X.shape[-1]):
+            X_sf = X[freq_idx,:,chan_idx]
+            score, permutation_scores, pvalue = permutation_test_score(clf, X=X_sf.reshape(-1, 1), y=y, groups=groups, cv=cv, n_permutations=n_perms, n_jobs=-1)
+            scores.append(score)
+            perm_scores.append(permutation_scores)
+            pvals.append(pvalue)
+            print(f'Computed feature {freq_idx} chan {chan_idx} Score {score}, pvalue {pvalue}')
+        all_scores.append(scores)
+        all_perm_scores.append(perm_scores)
+        all_pvals.append(pvals)
+        
+    all_scores = np.array(all_scores)
+    all_perm_scores = np.array(all_perm_scores)
+    all_pvals = np.array(all_pvals)
+    all_results = {'scores': all_scores, 'perm_scores': all_perm_scores, 'pvals': all_pvals}
+    return all_results
+
+def subject_average(X, y, groups):
+    """Computes group-averages."""
+    new_X = []
+    new_y = []
+    for subj in np.unique(groups):
+        for cond in np.unique(y):
+            new_X.append(np.nanmedian(X.transpose(1,0,2)[(groups == subj) & (y == cond)], axis=0))
+            new_y.append(cond)
+
+    new_X = np.array(new_X)
+    new_y = np.array(new_y)
+
+    return new_X, new_y
+
+def simple_contrast(X, y, groups):
+    """Computes subject-averages and contrasts between conditions."""
+    n_features = X.shape[0]
+    X_avg, y_avg = subject_average(X, y, groups)
+    # Average each condition separately
+    X_avg_by_cond = []
+    for cond in np.unique(y_avg):
+        X_avg_by_cond.append(np.nanmean(X_avg[y_avg == cond], axis=0))
+    # Compute normalized contrast (A - B)/B
+    X_contrast = (X_avg_by_cond[0] - X_avg_by_cond[1]) / X_avg_by_cond[1]
+    # Split conditions for ttest
+    X_condA = X_avg[y_avg == 0]
+    X_condB = X_avg[y_avg == 1]
+    
+    # Compute t-test
+    tvals = []
+    pvals = []
+    for feature_idx in range(n_features):
+        t, p = stats.ttest_rel(X_condB[:,feature_idx,:], X_condA[:,feature_idx,:], axis=0)
+        tvals.append(t)
+        pvals.append(p)
+    tvals = np.array(tvals)
+    pvals = np.array(pvals)
+    return X_contrast, tvals, pvals
+
+def subject_contrast(X, y):
+    n_features = X.shape[0]
+    X_avg_by_cond = []
+    X = X.transpose(1,0,2)
+    for cond in np.unique(y):
+        X_avg_by_cond.append(np.nanmean(X[y == cond], axis=0))
+    # Compute normalized contrast (A - B)/B
+    X_contrast = (X_avg_by_cond[1] - X_avg_by_cond[0]) / X_avg_by_cond[0]
+    # Split conditions for ttest
+    X_condA = X[y == 0]
+    X_condB = X[y == 1]
+    tvals = []
+    pvals = []
+    for feature_idx in range(n_features):
+        t, p = stats.ttest_ind(X_condA[:,feature_idx,:], X_condB[:,feature_idx,:], axis=0)
+        tvals.append(t)
+        pvals.append(p)
+    tvals = np.array(tvals)
+    pvals = np.array(pvals)
+    return X_contrast, tvals, pvals
+
+# Create significance mask
+def mask_pvals(pvals, alpha):
+    return pvals < alpha
+
+def apply_tmax(all_results):
+    da = all_results['scores']
+
+    da_perms = all_results['perm_scores']
+    da_perms = da_perms.reshape(da_perms.shape[0], -1)
+
+    tmax_pvals = np.empty_like(da)
+    for x in range(da.shape[0]):
+        for y in range(da.shape[1]):
+            pval = compute_pval(da[x,y], da_perms[x,:])
+            tmax_pvals[x,y] = pval
+    return tmax_pvals
+
+def compute_pval(score, perm_scores):
+    n_perm = len(perm_scores)
+    pvalue = (np.sum(perm_scores >= score) + 1.0) / (n_perm + 1)
+    return pvalue
