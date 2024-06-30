52114d3a447cbab28620e6cd953cfb99348f732c
Yann Harel <yharel109@gmail.com>
2024-06-30 15:16:40 +0900

    increased alloc

--- slurm/3_compute_features.sh
+++ slurm/3_compute_features.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-pbellec
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --job-name=safFeatures
 #SBATCH --mem=8G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=40
 
 $HOME/python_envs/saflow/bin/python $HOME/projects/def-kjerbi/hyruuk/cc_saflow/saflow/features/compute_$1.py -nt 8 -s $2 -r $3 -t alltrials
